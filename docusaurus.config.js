3e155c4b3c8e6e904a4164e6cd99f584397ff821
theQuert <a97041304@gmail.com>
2025-01-02 11:22:02 +0800

    Update copyright to 2025

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -202,7 +202,7 @@ const config = {
             ],
           },
         ],
-        copyright: `© 2024 Stima Tech All Rights Reserved.`,
+        copyright: `© 2025 Stima Tech All Rights Reserved.`,
       },
       prism: {
         theme: prismThemes.github,
